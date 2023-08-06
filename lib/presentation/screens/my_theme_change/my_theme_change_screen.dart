b1fc19a2da4d642a85565c19063f95219814b42d
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-08-06 18:12:24 -0500

    first commit

---
+++ lib/presentation/screens/my_theme_change/my_theme_change_screen.dart
@@ -0,0 +1,19 @@
+/* import 'package:flutter/material.dart';
+
+class MyThemeChangerScreen extends StatelessWidget {
+  static const String name = '/my_theme_changer_screen';
+  const MyThemeChangerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme Changer'),
+        actions: const [
+          
+        ],
+      ),
+    );
+  }
+}
+ */
